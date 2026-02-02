58cdc4070d98616f2b395bc62e6e5cab14505385
lukeharris3d <60995323+lukeharris3d@users.noreply.github.com>
2026-02-02 11:58:25 +1000

    Add SPLAT_DATA export with sandbar model

---
+++ splat_data.js
@@ -0,0 +1,8 @@
+export const SPLAT_DATA = [
+  {
+    name: "Sandbar",
+    model: "https://lukeharris3d.github.io/ARGS/sandbar.spz",
+    scale: { x: 1, y: 1, z: 1 },
+    rotation: { x: 180, y: 0, z: 0 },
+  },
+];
